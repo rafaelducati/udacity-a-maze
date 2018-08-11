655dd841b41552a13d48da65c83eea57a52921a3
Rafael Ducati <rafaelducati@gmail.com>
2018-08-10 23:53:06 -0300

    First commit

---
+++ vrnd-a-maze-by-rafael-ducati/Assets/UdacityVR/Scripts/Coin.cs
@@ -0,0 +1,101 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class Coin : MonoBehaviour {
+
+	// TODO: Create variables to reference the game objects we need access to
+	// Declare a GameObject named 'coinPoofPrefab' and assign the 'CoinPoof' prefab to the field in Unity
+	public GameObject coinPoofPrefab;
+
+	public float speedRotation = 100f;
+
+	public static bool collectCoins;
+
+	private static int count;
+
+	public Text countText;
+
+	private static string commentary;
+
+	public Text countCommentary;
+	
+	public void Start () {
+		count = 0;
+		SetTexts ();
+		
+	}
+
+
+	public void Update () {
+		// OPTIONAL-CHALLENGE: Animate the coin rotating
+		// TIP: You could use a method from the Transform class
+		transform.Rotate(Vector3.up, speedRotation * Time.deltaTime);
+		this.transform.position = new Vector3 (this.transform.position.x, this.transform.position.y + (Mathf.Sin (Time.time * 5.0f) / 50f), this.transform.position.z);
+
+	}
+
+
+	public void OnCoinClicked () {
+		/// Called when the 'Coin' game object is clicked - OK
+		/// Displays a poof effect (handled by the 'CoinPoof' prefab) - OK
+		/// Plays an audio clip (handled by the 'CoinPoof' prefab) - OK
+		/// Removes the coin from the scene - OK
+		/// Prints to the console when the method is called - OK
+
+		count++;
+
+		SetTexts ();
+
+		collectCoins = true;
+
+		Debug.Log("count: " + count + "/7");
+
+		Vector3 coinPos = transform.position;
+		Debug.Log("posicao da moeda: " + coinPos);
+		// TODO: Display the poof effect and remove the coin from the scene - OK
+		// Use Instantiate() to create a clone of the 'CoinPoof' prefab at this coin's position and with the 'CoinPoof' prefab's rotation - OK
+		GameObject newPoof = (GameObject)Object.Instantiate(coinPoofPrefab, new Vector3(coinPos[0], coinPos[1], coinPos[2]) , Quaternion.Euler(-90, 0, 0));
+		// Use Destroy() to delete the coin after for example 0.5 seconds - OK		
+		Destroy (this.gameObject, 0.5f);
+
+	}
+
+	public void SetTexts () {
+		// shows different comments at final signpost, depending on quanity of clicked coins
+		switch (count)
+		{
+		  case 1:
+		      commentary = "You did it.";
+		      break;
+		  case 2:
+		      commentary = "Keep going!";
+		      break;
+		  case 3:
+		      commentary = "Awesome.";
+		      break;
+		  case 4:
+		      commentary = "You win.";
+		      break;
+		  case 5:
+		      commentary = "Very good.";
+		      break;
+		  case 6:
+		      commentary = "Congrats.";
+		      break;
+		  case 7:
+		      commentary = "Perfect!";
+		      break;
+		  default:
+		      commentary = "Who needs money? ;)";
+		      break;
+		}
+
+		countText.text = "Coins: " + count.ToString() + "/7 ";
+		countCommentary.text = commentary;
+
+	}
+}
+
---
+++ vrnd-a-maze-by-rafael-ducati/Assets/UdacityVR/Scripts/Key.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Key : MonoBehaviour {
+
+	// TODO: Create variables to reference the game objects we need access to
+	// Declare a GameObject named 'keyPoofPrefab' and assign the 'KeyPoof' prefab to the field in Unity
+	public GameObject keyPoofPrefab;
+
+	// Declare a Door named 'door' and assign the top level 'Door' game object to the field in Unity
+	public GameObject door;
+
+	public float speedRotationKey = 100;
+
+	void Update () {
+		// OPTIONAL-CHALLENGE: Animate the key rotating
+		// TIP: You could use a method from the Transform class
+		transform.Rotate(Vector3.forward, speedRotationKey * Time.deltaTime);
+		this.transform.position = new Vector3 (this.transform.position.x, this.transform.position.y + (Mathf.Sin (Time.time * 5.0f) / 50f), this.transform.position.z);
+
+	}
+
+	
+	public void OnKeyClicked () {
+		/// Called when the 'Key' game object is clicked
+		/// - Unlocks the door (handled by the Door class)
+		/// - Displays a poof effect (handled by the 'KeyPoof' prefab)
+		/// - Plays an audio clip (handled by the 'KeyPoof' prefab)
+		/// - Removes the key from the scene
+
+
+		// TODO: Unlock the door, display the poof effect, and remove the key from the scene
+		// Use 'door' to call the Door.Unlock() method
+		door.GetComponent<Door>().Unlock();
+		door.GetComponent<Door>().foundKey = true; // testing var pass to another script
+		door.GetComponent<Door>().testMsg = "Passing testMsg"; // testing var pass to another script
+
+		
+		// Use Instantiate() to create a clone of the 'KeyPoof' prefab at this coin's position and with the 'KeyPoof' prefab's rotation
+		Vector3 keyPos = transform.position;
+		GameObject newKeyPoof = (GameObject)Object.Instantiate(keyPoofPrefab, new Vector3(keyPos[0], keyPos[1], keyPos[2]) , Quaternion.Euler(-90, 0, 0));
+		Debug.Log ("'foundKey' its " + door.GetComponent<Door>().foundKey);
+		Debug.Log ("'testMsg' its " + door.GetComponent<Door>().testMsg);
+		// Use Destroy() to delete the key after for example 0.5 seconds
+		Destroy (this.gameObject, 0.5f);
+	}
+}
